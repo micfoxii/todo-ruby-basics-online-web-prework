42ab154c1c55452ec65e1b15ea9ca735298df68c
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-11 01:05:10 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(variable_name="Bob")
+  return variable_name
 end
 
-def argue
-  
+def argue(phrase="I'm right and you are wrong!")
+  return phrase
 end
 
-def greeting
+def greeting(greeting, name)
+   
+  end
 
+def return_a_value(value="Nice")
+ puts value
+ return value
 end
 
-def return_a_value
-
+def last_evaluated_value (value="expert")
+ puts value
+ return value
 end
 
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party (phrase="cheese")
+return phrase
 end
