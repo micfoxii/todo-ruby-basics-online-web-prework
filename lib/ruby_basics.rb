fd31a34df41c41545d8e416526db4088cf2459f5
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-13 15:53:49 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,8 +10,12 @@ def argue(phrase="I'm right and you are wrong!")
   return phrase
 end
 
+<<<<<<< HEAD
 def greeting(greeting, name)
    
+=======
+def greeting(greeting="Hi there, ", name)
+>>>>>>> 5a02188482f4c6f0e97d4b423e3bae6995d76777
   end
 
 def return_a_value(value="Nice")
